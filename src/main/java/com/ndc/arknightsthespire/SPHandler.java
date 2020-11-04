d64fef617ea1b93c2eb05e45dfb819f801a32336
Kuiprux <kuiprux@gmail.com>
2020-11-04 23:04:56 +0900

    Adding SP -1

---
+++ src/main/java/com/ndc/arknightsthespire/SPHandler.java
@@ -0,0 +1,13 @@
+package com.ndc.arknightsthespire;
+
+public class SPHandler {
+    private int sp;
+
+    public int getSp() {
+        return sp;
+    }
+
+    public void addSp(int sp) {
+        this.sp = sp;
+    }
+}
---
+++ src/main/java/com/ndc/arknightsthespire/cards/CardSPBase.java
@@ -0,0 +1,19 @@
+package com.ndc.arknightsthespire.cards;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+
+public class CardSPBase extends CustomCard {
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+
+    }
+}
