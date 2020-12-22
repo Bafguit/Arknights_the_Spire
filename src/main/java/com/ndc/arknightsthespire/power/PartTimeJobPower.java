4acc3e847ceb5715ff62ba9ea3ca9a593d2e04e1
FastCat <bafguit9929@gmail.com>
2020-12-23 03:36:09 +0900

    추가 테스트 최종버전

--- src/main/java/com/ndc/arknightsthespire/power/PartTimeJobPower.java
+++ src/main/java/com/ndc/arknightsthespire/power/PartTimeJobPower.java
@@ -3,39 +3,39 @@ package com.ndc.arknightsthespire.power;
 import basemod.interfaces.CloneablePowerInterface;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.RegenPower;
 import com.ndc.arknightsthespire.util.TextureLoader;
 
 //Gain 1 dex for the turn for each card played.
 
-public class WeakeningPower extends AbstractPower implements CloneablePowerInterface {
+public class PartTimeJobPower extends AbstractPower implements CloneablePowerInterface {
     public AbstractCreature source;
     private boolean isLower;
-    private int number = 20;
 
-    public static final String POWER_ID = "ats:Weakening";
+    public static final String POWER_ID = "ats:Part Time Job";
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
     // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
     // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
-    private static final Texture tex84 = TextureLoader.getTexture("img/power/Weakening_84.png");
-    private static final Texture tex32 = TextureLoader.getTexture("img/power/Weakening_32.png");
+    private static final Texture tex84 = TextureLoader.getTexture("img/power/PartTimeJob_84.png");
+    private static final Texture tex32 = TextureLoader.getTexture("img/power/PartTimeJob_32.png");
 
-    public WeakeningPower(final AbstractCreature owner, final AbstractCreature source, int num) {
+    public PartTimeJobPower(final AbstractCreature owner, final AbstractCreature source, int num) {
         name = NAME;
         ID = POWER_ID;
 
         this.owner = owner;
         this.source = source;
-        this.number = num;
+        this.amount = num;
 
         type = PowerType.BUFF;
         isTurnBased = true;
@@ -68,11 +68,11 @@ public class WeakeningPower extends AbstractPower implements CloneablePowerInter
     // Update the description when you apply this power. (i.e. add or remove an "s" in keyword(s))
     @Override
     public void updateDescription() {
-        description = DESCRIPTIONS[0] + this.number + DESCRIPTIONS[1];
+        description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
     }
 
     @Override
     public AbstractPower makeCopy() {
-        return new WeakeningPower(owner, source, number);
+        return new PartTimeJobPower(owner, source, amount);
     }
 }
